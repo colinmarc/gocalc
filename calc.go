b8da5e1766a5ab8465ad55e1da0428358319caed
Colin Marc <colinmarc@gmail.com>
2012-11-18 23:23:17 -0800

    expression grouping

--- calc.go
+++ calc.go
@@ -6,6 +6,8 @@ import (
   "io"
   "bufio"
   "strings"
+  "math"
+  "strconv"
 //  "unicode/utf8"
 )
 
@@ -127,7 +129,108 @@ func lex(input string) *Lexer {
   return l
 }
 
+type Operator int
+const (
+  Power Operator = iota
+  Product
+  Division
+  Addition
+  Subtraction
+)
 
+var Operators = map[string]Operator {
+  "^": Power,
+  "*": Product,
+  "/": Division,
+  "+": Addition,
+  "-": Subtraction,
+}
+
+type Expression interface {
+  Evaluate() int
+}
+
+type ValueExpression struct {
+  value int
+}
+func (value *ValueExpression) Evaluate() int {
+  return int(value.value)
+}
+
+type NestedExpression struct {
+  left Expression
+  right Expression
+}
+
+type PowerExpression NestedExpression
+func (expr *PowerExpression) Evaluate() int {
+  left := float64(expr.left.Evaluate())
+  right := float64(expr.right.Evaluate())
+  return int(math.Pow(left, right))
+}
+
+type ProductExpression NestedExpression
+func (expr *ProductExpression) Evaluate() int {
+  return expr.left.Evaluate() * expr.right.Evaluate()
+}
+
+type DivisionExpression NestedExpression
+func (expr *DivisionExpression) Evaluate() int {
+  return expr.left.Evaluate() / expr.right.Evaluate()
+}
+
+type AdditionExpression NestedExpression
+func (expr *AdditionExpression) Evaluate() int {
+  return expr.left.Evaluate() + expr.right.Evaluate()
+}
+
+type SubtractionExpression NestedExpression
+func (expr *SubtractionExpression) Evaluate() int {
+  return expr.left.Evaluate() - expr.right.Evaluate()
+}
+
+func newExpression(oper Operator, left Expression, right Expression) Expression {
+  var expr Expression
+  switch oper {
+  case Power:
+    expr = &PowerExpression{left, right}
+  case Product:
+    expr = &ProductExpression{left, right}
+  case Division:
+    expr = &DivisionExpression{left, right}
+  case Addition:
+    expr = &AdditionExpression{left, right}
+  case Subtraction:
+    expr = &SubtractionExpression{left, right}
+  }
+
+  return expr
+}
+
+func groupLexemes(lexemes []Lexeme) Expression {
+  if len(lexemes) == 1 {
+    val, _ := strconv.Atoi(lexemes[0].value)
+    return &ValueExpression{val}
+  }
+
+  //janky
+  oper_idx := 1
+  lowest_oper := Operators[lexemes[1].value]
+  for i := 3; i+1 < len(lexemes); i += 2 {
+    oper := Operators[lexemes[i].value]
+    if oper < lowest_oper {
+      oper_idx = i
+    }
+  }
+
+  left := groupLexemes(lexemes[:oper_idx-1])
+  right := groupLexemes(lexemes[oper_idx+1:])
+  return newExpression(lowest_oper, left, right)
+}
+
+func parse(lexer *Lexer) {
+
+}
 
 func eval(line []byte) (int, error) {
   lexer := lex(string(line))
